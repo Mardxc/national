b274e97d9cf22c29b157fb3f58ab861a221d4f78
Mardxc <daryamz392@gmail.com>
2024-03-12 12:50:50 -0600

    Footer, who we are y hard facts

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
-angular.module("app", []).controller("AppController", function () {
-  const data = this;
-  data.msg = "msg";
+const ngApp = angular.module("app", []);
+
+ngApp.controller("AppController", function ($scope, $http) {
+  var employees  = this;
+
+  $http({
+    method: "GET",
+    url: "data/es.json",
+  }).then(
+    function (response) {
+      $scope.employees  = response.data;
+    },
+    function (error, status) {
+      console.error("Fail to load data", status, error);
+      $scope.data = {};
+    }
+  );
 });
